873434520aec1d1297c5408ac7a6f887622c8e56
Xuan Wu <xuan.wu@uky.edu>
2023-12-31 20:33:36 -0500

    update build script

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-# Unstructured Data Compression
+# Unstructured data compression
+Project: UMC
 
 # Installation
+git clone https://github.com/xuanwu02/unstructured_data_compression.git <br>
+cd unstructured_data_compression <br>
 sh build_script.sh <br>
-Dependancies: METIS, GSL, ZSTD, SZ3
 
 # Run compression
 ./build/test/test_compress data/coordinates.dat data/connectivity.dat 1 2 2 1 2 data/momentumX.dat data/velocityX.dat 0 1e-2 2 <br>
+
 Compressed files are data/momentumX.dat.umc and data/velocityX.dat.umc
 
 # Run decompression
 ./build/test/test_decompress data/coordinates.dat data/connectivity.dat 1 2 2 1 2 data/momentumX.dat data/velocityX.dat <br>
+
 Decompressed files are data/momentumX.dat.umc.out and data/velocityX.dat.umc.out
--- build_script.sh
+++ build_script.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
-src_dir=`pwd`
+source_dir=`pwd`
+external_dir=${source_dir}/external
+mkdir -p external
+
+# download METIS
+cd ${external_dir}
+git clone https://github.com/KarypisLab/METIS.git
+
+# build GSL
+curl -O https://ftp.gnu.org/gnu/gsl/gsl-2.7.1.tar.gz
+tar xfz gsl-2.7.1.tar.gz
+cd gsl-2.7.1
+mkdir -p gsl
+./configure --prefix=${source_dir}/external/gsl-2.7.1/gsl
+make -j 8
+make install
+
+# build SZ3
+cd ${external_dir}
+git clone https://github.com/szcompressor/SZ3.git
+cd SZ3
+cp ${source_dir}/SZ3_CMakeLists.txt CMakeLists.txt
+mkdir -p build 
+mkdir -p install
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=${external_dir}/SZ3/install -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ..
+make -j 8
+make install
 
 # build UMC
-cd ${src_dir}
-mkdir build
+cd ${source_dir}
+mkdir -p build
 cd build
-cmake ..
-make -j 4
+cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ..
+make -j 8
